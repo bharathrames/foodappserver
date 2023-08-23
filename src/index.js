532f3cc9f474a1a09b8d468714271f974477cf1a
bharathrames <bharath91505@gmail.com>
2023-08-23 07:46:38 +0530

    index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ import { recipesRouter } from "./routes/recipes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:3000"
+   //origin:"https://eqiumentrentalapp.netlify.app"
+}));
+
+app.get("/", function (req, res) {
+  res.send("<h5>Recepie app</h5>");
+});
 
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
