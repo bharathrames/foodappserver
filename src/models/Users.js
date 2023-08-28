c71b1e05dfcac9e512848f3919e5ddd420db2e96
bharathrames <bharath91505@gmail.com>
2023-08-28 10:35:45 +0530

    same

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,25 +1,8 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true,
-   
-    validate: {
-      validator: function (username) {
-        return username.length >= 4;
-      },
-      message: props => `Username must be at least 4 characters long!`
-    }
-  },
-  password: { type: String, required: true,
-  
-    validate: {
-      validator: function (password) {
-        return password.length >= 8;
-      },
-      message: props => `Password must be at least 8 characters long!`
-    }
-  
-  },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
 });
 
