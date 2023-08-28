ea30fc0c85ff2db49ecdb8875a93d44e33b4f887
bharathrames <bharath91505@gmail.com>
2023-08-28 10:22:06 +0530

    passowrdk

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true, unique: true, minLength: 4},
+  password: { type: String, required: true,  minLength: 8 },
   savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
 });
 
